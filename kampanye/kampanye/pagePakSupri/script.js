c4613becfbc671361e443b0fec9a02e1902268a4
Bagus Widiatmoko <98936410+Baguswmk@users.noreply.github.com>
2023-07-25 15:24:42 +0700

    Add files via upload

--- kampanye/kampanye/pagePakSupri/script.js
+++ kampanye/kampanye/pagePakSupri/script.js
@@ -4,6 +4,10 @@ const popup = document.getElementById("popup");
 const btnClose = document.getElementById("btnClose");
 const coblosElement = document.getElementById("coblos");
 const clickCountElement = document.getElementById("clickCount");
+const loginPopup= document.getElementById("login-popup");
+const loginOverlay = document.getElementById("login-overlay");
+const username = document.getElementById("username");
+const password = document.getElementById("password");
 
 // Fungsi untuk mengecek jumlah klik
 function checkClickCount() {
@@ -17,8 +21,10 @@ function checkClickCount() {
         clickCountElement.textContent = `${clickCount}`;
 
         if (clickCount === 0) {
+            // Tampilkan popup
+            showLoginPopup();
             // Redirect ke halaman login setelah countdown selesai
-            window.location.href = "login-page.html"; // Ganti "login-page.html" dengan halaman login yang sesuai
+            // window.location.href = "login-page.html"; // Ganti "login-page.html" dengan halaman login yang sesuai
         }
     };
 }
@@ -31,10 +37,26 @@ function showPopup() {
   popup.classList.remove("hidden");
 }
 
+function showLoginPopup() {
+  loginOverlay.style.display = "flex";
+  loginPopup.classList.remove("hidden");
+}
+
 // Fungsi untuk menyembunyikan popup
 function hidePopup() {
   overlay.style.display = "none";
   popup.classList.add("hidden");
+}
+
+function loginHidePopup() {
+  loginOverlay.style.display = "none";
+  loginPopup.classList.add("hidden");
+  window.location.href = "index.html";
+}
+
+
+function loginPage() {
+      window.location.replace("alogin.html");
 }
 
 // Ketika halaman dimuat, panggil fungsi showPopup() untuk menampilkan popup
@@ -42,5 +64,8 @@ document.addEventListener("DOMContentLoaded", showPopup);
 
 // Ketika tombol "Tutup" diklik, panggil fungsi hidePopup() untuk menyembunyikan popup
 btnClose.addEventListener("click", hidePopup);
+
+document.getElementById("login-button").addEventListener("click", loginPage);
+
 
 
